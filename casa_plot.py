8ac631888a5050de92e90bceb4f52d5abf4efe79
Charlie-Giese <giesec@tcd.ie>
2020-12-02 15:47:56 +0000

    added d_range

--- casa_plot.py
+++ casa_plot.py
@@ -17,7 +17,7 @@ from astropy.visualization import (MinMaxInterval, SqrtStretch,
 def args(argv):
 	arguments=['','','','']
 	try:
-		opts, args = getopt.getopt(argv,"hi:p:c:o:")
+		opts, args = getopt.getopt(argv,"hi:d:c:o:")
 	except 	getopt.GetoptError:
 		print('casa_plot.py -i <inputfile> -p <power_scale_factor> -c <contour_bias_factor> -o <outputfile')
 		sys.exit(2)
@@ -25,26 +25,34 @@ def args(argv):
 		if opt == '-h':
 			print('Usage:')
 			print('-i <inputfile>, required. Path to fits file relative to current working directory.')
-			print('-p <scale_power_factor>, required. 0 for linear scaling, negative value to make make faint features brighter and vice versa')
+			print('-d <data_range>, the data range you want to plot, e.g. "-d 0,1"')
 			print('-c <contour_bias_factor>, optional. If blank, no contours. Takes values in range [0-inf]. Value of 0 results in contours biased towards maximum of data range, higher values scale towards min')
 			print('-o <outputfile>, optional. Name of output png file, if blank does not save image')
 			sys.exit()
 		elif opt in ('-i'):
 			inputfile = arg
 			arguments[0] = arg
-		elif opt in ('-p'):
-			p = arg
-			arguments[1] = arg
+		elif opt in ('-d'):
+			d_range=[arg]
+			arguments[1] = d_range
 		elif opt in ('-c'):
 			c = arg
 			arguments[2] = arg
+			if c != '':
+				contours=True
+			else:
+				contours=False
 		elif opt in ('-o'):
 			outputfile = arg
 			arguments[3] = outputfile
+			if outputfile != '':
+				savefile=True
+			else:
+				savefile=False
 	return arguments
 
 
-def import_fits(filename):
+def import_fits(filename, d_range):
 	"""Imports a FITS radio image from CASA pipeline """
 	
 	fits_image_filename = os.getcwd()+filename
@@ -59,36 +67,19 @@ def import_fits(filename):
 	fwhm_to_sigma = 1. / (8 * np.log(2))**0.5
 	beam_sigma = beam_fwhm * fwhm_to_sigma
 	omega_B = 2 * np.pi * beam_sigma**2
-	surface_brightness=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(omega_B)))[0,0,:,:]
-	print(np.max(surface_brightness))
-	return surface_brightness
-
-
-def power_scale(b_map, p, d_range):
-	"""Takes as inputs a surface brightness map in Jy/str, a value for the power scaling and a data_range (list)
-	   First, data is clipped so it only lies within the given data range. It is then scaled to lie between 0 and
-	   and 10^p where p is the scale. Then take log of these values, giving a range of 1-p. These values will be 
-	   scaled linearly to the set of availabel colors. (NOTE: This assumes negative values of p, can also take 
-	   positive values, not described here"""
+	SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(omega_B)))[0,0,:,:].value
 	
-	data=b_map.value #[0,0,:,:] only keep dimensions we want
+	"""SB is a 2-d array with Surface_brigthness values in MJy/sr. For masked arrays, some values are nan, need to create a mask to 
+	these values. Before that, need to set all values not within data range to NAN. Using SB.data can access the original array."""
 
 
-	if p < 0:
-		power=-1.*p
-		scaled_data=((data-np.min(data))/np.max(data))*(10**power)
-		#print(np.min(scaled_data), np.max(scaled_data), scaled_data)
-		output=np.log10(scaled_data+1)
-		#print(np.min(output), np.max(output), output)
-		return output
-	elif p >0:
-		scaled_data=((data-np.min(data))/np.max(data))*(p)
-		output=10**scaled_data
-		return output
-	else:
-		return data
+	if d_range != '':
+		SB[SB < d_range[0] && SB > d_range[1]] = np.nan
+	
+	sb_maskedNAN = np.ma.array(SB, mask=np.isnan(SB))	
+	return sb_maskedNAN
 
-		
+
 def image_plot(image_data, contours, c, savefile, outputfile):
 	"""Takes a scaled image in the form of a numpy array and plots it along with coordinates and a colorbar
 	   The contours option takes a boolean"""
@@ -99,6 +90,7 @@ def image_plot(image_data, contours, c, savefile, outputfile):
 	wcs = WCS(hrd)
 	
 	data=image_data.value
+	
 	
 	norm = ImageNormalize(data, interval=MinMaxInterval(),
                       stretch=SqrtStretch())
@@ -129,7 +121,7 @@ def image_plot(image_data, contours, c, savefile, outputfile):
 def emission_measure(S):
 	T=8000 #K
 	v=8e9 # central frequency of our broadband observations
-	S_erg = S * 10**-23 #this is the surface brightness in units of ergs/cm^2/sr
+	S_erg = S * 10**-17	 #this is the surface brightness in units of ergs/cm^2/sr
 	c=3e10 #speed of light in cgs
 	k_b=1.38e-16 #boltzmann constant in cgs
 	emission_measure = -1 *np.log(1 - ((S_erg*c**2)/(2*k_b*T*(v**2)))) * 1/(3.28e-7) * (T/1e4)**1.35 * (v/1e9)**2.1	
@@ -153,24 +145,13 @@ def emission_measure(S):
 arguments = args(sys.argv[1:])
 
 inputfile = arguments[0]
-p = int(arguments[1])
+d_range = arguments[1]
 c = arguments[2]
 outputfile = arguments[3]
 
-if c != '':
-	contours=True
-else:
-	contours=False
-
-if outputfile != '':
-	savefile=True
-else:
-	savefile=False
-
-b_map=import_fits(inputfile)
+b_map=import_fits(inputfile, d_range)
 lower_lim=np.min(b_map)
 upper_lim=np.max(b_map) 
-#image=power_scale(b_map, p, [lower_lim, upper_lim]) 
 c=image_plot(b_map, contours, c, savefile, outputfile)
 em=emission_measure(b_map.value)
 
@@ -178,6 +159,36 @@ em=emission_measure(b_map.value)
 
 
 
+
+
+
+
+
+
+# THIS FUNCTION IS NO LONGER USED
+def power_scale(b_map, p, d_range):
+	"""Takes as inputs a surface brightness map in Jy/str, a value for the power scaling and a data_range (list)
+	   First, data is clipped so it only lies within the given data range. It is then scaled to lie between 0 and
+	   and 10^p where p is the scale. Then take log of these values, giving a range of 1-p. These values will be 
+	   scaled linearly to the set of availabel colors. (NOTE: This assumes negative values of p, can also take 
+	   positive values, not described here"""
+	
+	data=b_map.value #[0,0,:,:] only keep dimensions we want
+
+
+	if p < 0:
+		power=-1.*p
+		scaled_data=((data-np.min(data))/np.max(data))*(10**power)
+		#print(np.min(scaled_data), np.max(scaled_data), scaled_data)
+		output=np.log10(scaled_data+1)
+		#print(np.min(output), np.max(output), output)
+		return output
+	elif p >0:
+		scaled_data=((data-np.min(data))/np.max(data))*(p)
+		output=10**scaled_data
+		return output
+	else:
+		return data
 
 
 
