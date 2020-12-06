abb1a21f4fe6de7a2a4390d6ceb04f0116cee8d3
Charlie-Giese <giesec@tcd.ie>
2020-12-06 17:37:20 +0000

    tweaks

--- casa_plot.py
+++ casa_plot.py
@@ -17,7 +17,7 @@ from radio_beam import Beam
 parser = argparse.ArgumentParser(description='Plot an image in fits format.')
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument("-i", "--inputfile", help="Path of input fits file relative to current working directory")
-parser.add_argument("-cf", "--contour_file", help"fits file to plot contours from")
+parser.add_argument("-cf", "--contour_file", help="fits file to plot contours from", default=False)
 parser.add_argument("-ct", "--contour_type", help="automatic (contour_bias) or defined (contour_levels)") 
 parser.add_argument("-cb", "--contour_bias", help="value of contour level bias")
 parser.add_argument("-cl", "--contour_levels", nargs="+", help="sigma levels to plot contours at")
@@ -26,9 +26,9 @@ parser.add_argument("-o", "--outputfile", help="name of output .png file (option
 parser.add_argument("-e", "--emission_measure", help="Optional emission measure plot, True/False", default=False)
 args = parser.parse_args()
 inputfile=args.inputfile
+
 contour_file=args.contour_file
-contour_type=str(args.contour_type)
-contour_levels=list(map(float, args.contour_levels))
+contour_type=args.contour_type
 contour_bias=args.contour_bias
 d_range=args.d_range
 outputfile=args.outputfile
@@ -45,9 +45,16 @@ def import_fits(filename, d_range):
 		data=hdul[0].data
 
 	header = fits.getheader(fits_image_filename)
-	beam = Beam.from_fits_header(header)
+	if header['BUNIT'] == 'Jy/beam':
+		beam = Beam.from_fits_header(header)
+		SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
+	elif header['BUNIT'] == 'JY/BEAM':
+		beam = Beam.from_fits_header(header)
+		SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
+	elif header['BUNIT'] == 'MJy/sr':
+		SB=data
 
-	SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
+	
 	"""SB is a 2-d array with Surface_brigthness values in MJy/sr. For masked arrays, some values are nan, need to create a mask to 
 	these values. Before that, need to set all values not within data range to NAN. Using SB.data can access the original array."""
 	
@@ -69,19 +76,14 @@ def import_contours(filename):
 		data=hdul[0].data
 
 	header = fits.getheader(fits_image_filename)
-	try:
+	if header['BUNIT'] == 'Jy/beam':
 		beam = Beam.from_fits_header(header)
-	except:
-		continue
-	#now convert to Jy/str surface brigthness
-	#fwhm_to_sigma = 1. / (8 * np.log(2))**0.5
-	#beam_fwhm_x = beam[0]*u.arcsec
-	#beam_sigma_x = beam_fwhm_x * fwhm_to_sigma
-	#beam_fwhm_y = beam[1]*u.arcsec
-	#beam_sigma_x = beam_fwhm_x * fwhm_to_sigma
-	#omega_B = 2 * np.pi * (beam_sigma_x**2 + beam_sigma_y**2)
-	#SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(omega_B)))[0,0,:,:].value
-	SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
+		SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
+	elif header['BUNIT'] == 'JY/BEAM':
+		beam = Beam.from_fits_header(header)
+		SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
+	elif header['BUNIT'] == 'MJy/sr':
+		SB=data
 	"""SB is a 2-d array with Surface_brigthness values in MJy/sr. For masked arrays, some values are nan, need to create a mask to 
 	these values. Before that, need to set all values not within data range to NAN. Using SB.data can access the original array."""
 	
@@ -106,6 +108,7 @@ def image_plot(inputfile, d_range, outputfile):
 	hdu = fits.open(os.getcwd()+inputfile)
 	hrd=hdu[0].header
 	wcs = WCS(hrd)
+
 	
 	data=import_fits(inputfile, d_range)
 
@@ -114,25 +117,33 @@ def image_plot(inputfile, d_range, outputfile):
                       stretch=SqrtStretch())
 	
 	figure=plt.figure(num=1)
-	ax=figure.add_subplot(111, projection=wcs, slices=('x','y',0,0))
+	if wcs.pixel_n_dim == 4:
+		ax=figure.add_subplot(111, projection=wcs, slices=('x','y', 0, 0))
+	elif wcs.pixel_n_dim ==2:
+		ax=figure.add_subplot(111, projection=wcs, slices=('x','y'))
 	main_image=ax.imshow(X=data, cmap='plasma', origin='lower', norm=norm, vmax=np.max(data)- 5 , vmin=np.min(data))
 	cbar=figure.colorbar(main_image)
 	ax.set_xlabel('Right Ascension J2000')
 	ax.set_ylabel('Declination J2000')
+	ax.set_xlim(
 	cbar.set_label('Surface Brigthness (MJy/Sr)')
-	
+	if contour_file != False:
+		contours=contour_plot(ax, contour_file, contour_type)
 	plt.show()
 	if outputfile != False:
 		plt.savefig(os.getcwd()+outputfile)
-		
-def contour_plot(ax, contour_file, contour_type)
+
+def contour_plot(ax, contour_file, contour_type):
 
 
 	hdu = fits.open(os.getcwd()+contour_file)
 	hrd=hdu[0].header
 	wcs = WCS(hrd)
-	contour_data=import_contours(os.getcwd()+contour_file)
-	
+	contour_data=import_contours(contour_file)
+	contour_type=str(contour_type)
+	contour_levels=list(map(float, args.contour_levels))
+	data=import_fits(inputfile, d_range)
+	ax.set_autoscale_on(False)
 
 	if contour_type == "automatic":
 		spacing = contour_bias
@@ -144,6 +155,7 @@ def contour_plot(ax, contour_file, contour_type)
 			dx = 1.0 / (n-1)
 			return [lb + (i*dx)**spacing*span for i in range(n)]
 		levels=level_func(lb, ub, n, spacing=float(c))
+		print('Generated Levels for contour are: ', levels)
 		ax.contour(contour_data, levels=levels, colors='white', alpha=0.5)
 	if contour_type == "defined":
 		sigma_levels=np.array(contour_levels)
@@ -153,31 +165,9 @@ def contour_plot(ax, contour_file, contour_type)
 		ax.contour(contour_data, levels=levels, colors='white', alpha=0.5)
 
 
-def em():
-	S=import_fits(inputfile, d_range)
-
-	T=8000 #K
-	v=8e9 # central frequency of our broadband observations
-	S_erg = S * 10**-17	 #this is the surface brightness in units of ergs/cm^2/sr
-	c=3e10 #speed of light in cgs
-	k_b=1.38e-16 #boltzmann constant in cgs
-	emission_measure = -1 *np.log(1 - ((S_erg*c**2)/(2*k_b*T*(v**2)))) * 1/(3.28e-7) * (T/1e4)**1.35 * (v/1e9)**2.1	
-	
-	hdu = fits.open(os.getcwd()+inputfile)
-	hrd=hdu[0].header
-	wcs = WCS(hrd)
-	
-	fig=plt.figure(1) 
-	ax=fig.add_subplot(111, projection=wcs, slices=('x','y',0,0))
-	em_map=ax.imshow(emission_measure, origin='lower')
-	ax.set_xlabel('Right Ascension')
-	ax.set_ylabel('Declination')
-	cbar=fig.colorbar(em_map)
-	cbar.set_label('Emission Measure')
-	plt.show()
 	
 
-result=image_plot(inputfile, d_range, contour_type, outputfile)
+result=image_plot(inputfile, d_range,  outputfile)
 
 if emission_measure != False:
 	result=em()
