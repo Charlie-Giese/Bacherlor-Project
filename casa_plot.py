a658cf669d4d6d23862815e06713d8175d2fd153
Charlie-Giese <giesec@tcd.ie>
2020-12-09 10:49:53 +0000

    fixed contour plotting

--- casa_plot.py
+++ casa_plot.py
@@ -23,17 +23,19 @@ parser.add_argument("-cb", "--contour_bias", help="value of contour level bias")
 parser.add_argument("-cl", "--contour_levels", nargs="+", help="sigma levels to plot contours at")
 parser.add_argument("-d", "--d_range", nargs="+", help="range of data to plot", default=False)
 parser.add_argument("-o", "--outputfile", help="name of output .png file (optional)", default=False)
-parser.add_argument("-e", "--emission_measure", help="Optional emission measure plot, True/False", default=False)
 args = parser.parse_args()
 inputfile=args.inputfile
 
 contour_file=args.contour_file
 contour_type=args.contour_type
 contour_bias=args.contour_bias
+
 d_range=args.d_range
 outputfile=args.outputfile
-emission_measure=args.emission_measure
 
+
+
+### IMAGE PLOT CODE; INCLUDES A FUNCTION FOR IMPORTING FITS FILE, CONVERTING TO APPROPRIATE UNITS AND THEN PLOTTING ###
 
 
 def import_fits(filename, d_range):
@@ -67,8 +69,47 @@ def import_fits(filename, d_range):
 	
 	return array
 	
+def image_plot(inputfile, d_range, outputfile):
+	"""Takes a scaled image in the form of a numpy array and plots it along with coordinates and a colorbar"""
+	
+	hdu = fits.open(os.getcwd()+inputfile)
+	hrd=hdu[0].header
+	wcs = WCS(hrd)
+	
+	data=import_fits(inputfile, d_range)
+	
+	#if d_range != False:
+	norm = ImageNormalize(data, interval=MinMaxInterval(), stretch=SqrtStretch())
+	#else:
+	#	norm = ImageNormalize(data, vmin=d_range[0], vmax=d_range[1], stretch=SqrtStretch())
+	
+	figure=plt.figure(num=1)
+	if wcs.pixel_n_dim == 4:
+		ax=figure.add_subplot(111, projection=wcs, slices=('x','y', 0, 0))
+	elif wcs.pixel_n_dim ==2:
+		ax=figure.add_subplot(111, projection=wcs, slices=('x','y'))
+	main_image=ax.imshow(X=data, cmap='plasma', origin='lower', norm=norm, vmax=np.max(data)- 5 , vmin=np.min(data))
+	cbar=figure.colorbar(main_image)
+	if hrd['TELESCOP'] == 'Spitzer':
+		ax.invert_xaxis()
+		ax.invert_yaxis()
+	ax.set_xlabel('Right Ascension J2000')
+	ax.set_ylabel('Declination J2000')
+	cbar.set_label('Surface Brigthness (MJy/Sr)')
+	if contour_file != False:
+		image_beam = Beam.from_fits_header(hrd)
+		contours=contour_plot(ax, contour_file, contour_type, image_beam)
+	plt.show()
+	if outputfile != False:
+		plt.savefig(os.getcwd()+outputfile)
+
+
+### CODE FOR IMPORTING A FITS FILE FOR CONTOURS AND THEN OVERPLOTTING THOSE CONTOURS ON THE IMAGE CREATED ABOVE ###
+
+
+
 def import_contours(filename):
-	"""Imports a FITS radio image from CASA pipeline"""
+	"""Imports a FITS radio image from CASA pipeline, and coverts units to MJy/sr if not already in those units."""
 	
 	fits_image_filename = os.getcwd()+filename
 	with fits.open(fits_image_filename) as hdul:
@@ -83,68 +124,31 @@ def import_contours(filename):
 		beam = Beam.from_fits_header(header)
 		SB=((data*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam)))[0,0,:,:].value
 	elif header['BUNIT'] == 'MJy/sr':
-		SB=data
+		SB=data*(u.MJy/u.sr)
 	"""SB is a 2-d array with Surface_brigthness values in MJy/sr. For masked arrays, some values are nan, need to create a mask to 
 	these values. Before that, need to set all values not within data range to NAN. Using SB.data can access the original array."""
+	print('The contour data is in units of ', SB.unit)
+	SB=SB.value
 	
-	
-	
-	sb_maskedNAN = np.ma.array(SB, mask=np.isnan(SB))	
-	if d_range != False:
-		sb_maskedNAN[(sb_maskedNAN < np.float32(d_range[0]))] = d_range[0]
-		sb_maskedNAN[(sb_maskedNAN > np.float32(d_range[1]))] = d_range[1]
-	
-	array = np.ma.array(sb_maskedNAN, mask=np.isnan(sb_maskedNAN))	
+	array = np.ma.array(SB, mask=np.isnan(SB))	
 	
 	return array
 
 
-def image_plot(inputfile, d_range, outputfile):
-	"""Takes a scaled image in the form of a numpy array and plots it along with coordinates and a colorbar
-	   The contours option takes a boolean"""
-	
-	
-	
-	hdu = fits.open(os.getcwd()+inputfile)
-	hrd=hdu[0].header
-	wcs = WCS(hrd)
 
-	
-	data=import_fits(inputfile, d_range)
+def contour_plot(ax, contour_file, contour_type, beam):
+	"""Plots contours over the main image, can be either from the same image or from a different one, fits file."""
 
 
-	norm = ImageNormalize(data, interval=MinMaxInterval(),
-                      stretch=SqrtStretch())
-	
-	figure=plt.figure(num=1)
-	if wcs.pixel_n_dim == 4:
-		ax=figure.add_subplot(111, projection=wcs, slices=('x','y', 0, 0))
-	elif wcs.pixel_n_dim ==2:
-		ax=figure.add_subplot(111, projection=wcs, slices=('x','y'))
-	main_image=ax.imshow(X=data, cmap='plasma', origin='lower', norm=norm, vmax=np.max(data)- 5 , vmin=np.min(data))
-	cbar=figure.colorbar(main_image)
-	ax.set_xlabel('Right Ascension J2000')
-	ax.set_ylabel('Declination J2000')
-	ax.set_xlim(
-	cbar.set_label('Surface Brigthness (MJy/Sr)')
-	if contour_file != False:
-		contours=contour_plot(ax, contour_file, contour_type)
-	plt.show()
-	if outputfile != False:
-		plt.savefig(os.getcwd()+outputfile)
-
-def contour_plot(ax, contour_file, contour_type):
-
-
-	hdu = fits.open(os.getcwd()+contour_file)
-	hrd=hdu[0].header
-	wcs = WCS(hrd)
-	contour_data=import_contours(contour_file)
+	hdu_c = fits.open(os.getcwd()+contour_file)
+	hrd_c=hdu_c[0].header
+	wcs_c = WCS(hrd_c)
+	contour_data=import_contours(contour_file)	
+	print(np.max(contour_data))
 	contour_type=str(contour_type)
-	contour_levels=list(map(float, args.contour_levels))
-	data=import_fits(inputfile, d_range)
 	ax.set_autoscale_on(False)
-
+	data=import_fits(contour_file, d_range)
+	
 	if contour_type == "automatic":
 		spacing = contour_bias
 		n = 7 #number of contours
@@ -154,23 +158,22 @@ def contour_plot(ax, contour_file, contour_type):
 			span = (ub-lb)
 			dx = 1.0 / (n-1)
 			return [lb + (i*dx)**spacing*span for i in range(n)]
-		levels=level_func(lb, ub, n, spacing=float(c))
-		print('Generated Levels for contour are: ', levels)
-		ax.contour(contour_data, levels=levels, colors='white', alpha=0.5)
+		levels=level_func(lb, ub, n, spacing=float(spacing))
+		print('Generated Levels for contour are: ', levels, 'MJy/sr')
+		ax.contour(contour_data, levels=levels, colors='white', transform=ax.get_transform(wcs_c.celestial), alpha=0.5)
 	if contour_type == "defined":
+		contour_levels=list(map(float, args.contour_levels))
 		sigma_levels=np.array(contour_levels)
-		rms=np.sqrt(np.mean(np.square(contour_data)))
+		rms=(0.00004*u.Jy/u.beam).to(u.MJy/u.sr, equivalencies=u.beam_angular_area(beam))
 		levels=rms*sigma_levels
-		print('Sigma Levels for contour are: ', levels)
-		ax.contour(contour_data, levels=levels, colors='white', alpha=0.5)
+		print('Sigma Levels for contour are: ', sigma_levels, 'sigma')
+		ax.contour(contour_data, levels=levels, transform=ax.get_transform(wcs_c.celestial), colors='white', alpha=0.5)
 
 
 	
 
 result=image_plot(inputfile, d_range,  outputfile)
 
-if emission_measure != False:
-	result=em()
 
 
 
