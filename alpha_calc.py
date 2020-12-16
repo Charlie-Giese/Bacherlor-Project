8f5486fc2adcefc29ea4a55939070820338538fc
Charlie-Giese <giesec@tcd.ie>
2020-12-16 14:51:12 +0000

    update

--- alpha_calc.py
+++ alpha_calc.py
@@ -5,7 +5,7 @@
 
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from matplotlib.font_manager import FontProperties 
+from matplotlib.font_manager import FontProperties
 import pandas as pd
 import numpy as np
 import glob
@@ -19,12 +19,13 @@ from astropy.io import fits
 
 
 images = [
-os.getcwd()+'/fits/ngc7635_4-5_wide_g0.5_briggs1.050iter_inter.image.5sigmasked.fits',
-os.getcwd()+'/fits/ngc7635_5-6_wide_g0.5_briggs1.050iter_inter.image.5sigmasked.fits',
-os.getcwd()+'/fits/ngc7635_6-7_wide_g0.5_briggs1.050iter_inter.image.5sigmasked.fits',
-os.getcwd()+'/fits/ngc7635_8-9_wide_g0.5_briggs1.050iter_inter.image.5sigmasked.fits',
-os.getcwd()+'/fits/ngc7635_9-10_wide_g0.5_briggs1.050iter_inter.image.5sigmasked.fits',
-os.getcwd()+'/fits/ngc7635_10-11_wide_g0.5_briggs1.050iter_inter.image.5sigmasked.fits'
+'alpha_fits/BN_4_5smoothed.fits',
+'alpha_fits/BN_5_6smoothed.fits',
+'alpha_fits/BN_6_7smoothed.fits',
+'alpha_fits/BN_7_8smoothed.fits',
+'alpha_fits/BN_8_9smoothed.fits',
+'alpha_fits/BN_9_10smoothed.fits',
+'alpha_fits/BN_10_11smoothed.fits'
 ]
 
 
@@ -32,11 +33,10 @@ rms = [
 0.0001, # 4-5
 0.00015, # 5-6
 0.00013, # 6-7
-#0.0001, # 7-8
+0.0001, # 7-8
 0.0001, # 8-9
 0.0001, # 9-10
 0.0001, # 10-11
-#5e-5 # 11-12
 ]
 
 
@@ -49,13 +49,13 @@ def jyb_to_mjsr(bmin,bmaj):
 
 def s_nu(nu,s0,alpha): # f(xdata,a0,a1)
   return s0*nu**alpha
-  
+
 def em_mes(nu,s0):
   t0     = 8e3       # temperature K
   a      = 2*(k_B.cgs)*3.28*1e-7/(c.cgs)**2
   tau_nu2_T_em = a*1e4*(t0/1e4)**(-0.35)*1e18*(nu)**(-0.1)
   return (s0/tau_nu2_T_em*1e-23).value
-  
+
 
 regions = open('alpha_regions.txt', 'r')
 print regions
@@ -69,10 +69,10 @@ for reg in regions.readlines():
   yerr   = []
   em     = []
   for im in images:
-  
+
      header = fits.getheader(im)
-     
-		
+
+
      bmaj  = imhead(im,mode='get',hdkey='beammajor')['value']*u.arcsec
      bmin  = imhead(im,mode='get',hdkey='beamminor')['value']*u.arcsec
      equiv = jyb_to_mjsr(bmin,bmaj)
@@ -85,66 +85,107 @@ for reg in regions.readlines():
      except:
        continue
 
-	
+
   #area = (reg.split(',')[-3])
-  
+
   data_all = pd.DataFrame({'Frequencies (GHz)':np.round(freqs,3), 'Fluxes (MJy sr-1)':np.round(fluxes,3),
                            'RMS (Jy sr-1)': np.round(yerr,3) , 'EM (pc cm-6)': np.round(em,3)},
                            columns=['Frequencies (GHz)', 'Fluxes (MJy sr-1)', 'RMS (Jy sr-1)', 'EM (pc cm-6)'])
-  
-  
+
+
   #print(area)
   #print(data_all)
-  
+
   #fig=plt.figure()
   #ax=fig.add_subplot(111)
   #ax.scatter(freqs, fluxes)
   #plt.show()
-  
-  
-  
 
-  coef, cov = curve_fit(s_nu,freqs,fluxes,sigma=yerr,absolute_sigma=True)    #cf(f,xdata,ydata) 
-  s0        = coef[0] 
+
+
+
+  coef, cov = curve_fit(s_nu,freqs,fluxes,sigma=yerr,absolute_sigma=True)    #cf(f,xdata,ydata)
+  s0        = coef[0]
   freq_err  = np.sqrt(cov[0,0]) # [[sxx sxy][syx syy]]
   alpha     = coef[1]
   alpha_err = np.sqrt(cov[1,1])
-  
-  
-  lgfr  = np.log10(np.asarray(freqs))
-  lgfl  = np.log10(np.asarray(fluxes))
 
-  
-  y  = s_nu(freqs,s0,alpha) 
-  
-  yerr_fixed = np.array(yerr)
-  yerr_fixed[yerr>=y] = y[yerr>=y]*.9999 # fix the values that are way too large 
+  print(alpha)
+
+
+  y  = s_nu(freqs,s0,alpha)
+  yerr_fixed = np.array(yerr) * 3
+
 
   ypm = s_nu(freqs,coef[0]+freq_err,coef[1]-alpha_err)
   ypp = s_nu(freqs,coef[0]+freq_err,coef[1]+alpha_err)
   yp  = np.maximum(ypm,ypp)
-  
+
   ymp = s_nu(freqs,coef[0]-freq_err,coef[1]+alpha_err)
   ymm = s_nu(freqs,coef[0]-freq_err,coef[1]-alpha_err)
   ym  = np.minimum(ymp,ymm)
 
-  
-  #print('freq:',lgfr,'fluxes:',lgfl,'yerr:',yerr,'fit:',y)
   fig = plt.figure()
-  ax  = plt.axes()
-  ax.set_xscale("log"); ax.set_yscale("log",nonposy="clip")
-  
+  ax = fig.add_subplot(111)
+  ax.set_xscale("log"); ax.set_yscale("log")
+  #ax.scatter(freqs, fluxes)
+  ax.plot(freqs, y, c='k', label=r'$\alpha$'+'='+str(round(coef[1],2))+' +/-'+str(round(alpha_err,2)))
+
+  ax.plot(freqs,yp,'g--')
+  ax.plot(freqs,ym,'g--')
+
+  ax.fill_between(freqs,yp,ym,facecolor='gray',alpha=0.15)
+
+  ax.set_xlim([2,12.6])
+  #ax.set_ylim([0.1, 8])
+
+
   ax.errorbar(freqs,fluxes,
   yerr=yerr_fixed,
   xerr=0.5,
   fmt='r+',label='data')
-  	
-  ax.plot(freqs,y,label= r'$\alpha$'+'='+str(round(coef[1],2))+' +/-'+str(round(alpha_err,2))) 
-  
+
+  plt.legend(loc='lower right')
+  ax.set_xlabel('Frequency (GHz)')
+  ax.set_ylabel('Surface Brightness (MJy/sr)')
+
+  plt.show()
+
+
+
+
+"""
+  y  = s_nu(freqs,s0,alpha)
+
+  yerr_fixed = np.array(yerr)
+
+  yerr_fixed[yerr>=y] = y[yerr>=y]*.9999 # fix the values that are way too large
+
+  ypm = s_nu(freqs,coef[0]+freq_err,coef[1]-alpha_err)
+  ypp = s_nu(freqs,coef[0]+freq_err,coef[1]+alpha_err)
+  yp  = np.maximum(ypm,ypp)
+
+  ymp = s_nu(freqs,coef[0]-freq_err,coef[1]+alpha_err)
+  ymm = s_nu(freqs,coef[0]-freq_err,coef[1]-alpha_err)
+  ym  = np.minimum(ymp,ymm)
+
+
+  #print('freq:',lgfr,'fluxes:',lgfl,'yerr:',yerr,'fit:',y)
+  fig = plt.figure()
+  ax  = plt.axes()
+  ax.set_xscale("log"); ax.set_yscale("log",nonposy="clip")
+
+  ax.errorbar(freqs,fluxes,
+  yerr=yerr_fixed,
+  xerr=0.5,
+  fmt='r+',label='data')
+
+  ax.plot(freqs,y,label= r'$\alpha$'+'='+str(round(coef[1],2))+' +/-'+str(round(alpha_err,2)))
+
   ax.plot(freqs,yp,'g--')
   ax.plot(freqs,ym,'g--')
   ax.fill_between(freqs,yp,ym,facecolor='gray',alpha=0.15)
-  
+
   ax.set_xlim([4.4,11.6])
   ax.set_ylim([1e-5,1e-2])
   ax.set_ylabel(r'$F$ (Jy)')
@@ -152,9 +193,8 @@ for reg in regions.readlines():
   #ax.set_title()
   fp = FontProperties()
   fp.set_size('x-small')
-  plt.legend(title=area,#bbox_to_anchor=(1.007,1.14),
-  	loc='lower right',prop=fp,frameon=False)
-  
-  plt.show()
-  
+  #plt.legend(title=area,#bbox_to_anchor=(1.007,1.14),
+  	#loc='lower right',prop=fp,frameon=False)
 
+  plt.show()
+"""
--- image_plot.py
+++ image_plot.py
@@ -9,7 +9,7 @@ import os
 from astropy.coordinates import SkyCoord
 import sys, getopt
 from astropy.wcs import WCS
-from matplotlib import cm	
+from matplotlib import cm
 from astropy.visualization import (MinMaxInterval, SqrtStretch,
                                    ImageNormalize)
 import argparse
@@ -19,10 +19,11 @@ parser = argparse.ArgumentParser(description='Plot an image in fits format.')
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument("-i", "--inputfile", help="Path of input fits file relative to current working directory")
 parser.add_argument("-cf", "--contour_file", help="fits file to plot contours from", default=False)
-parser.add_argument("-ct", "--contour_type", help="automatic, sigma or manual") 
+parser.add_argument("-ct", "--contour_type", help="automatic, sigma or manual")
 parser.add_argument("-cb", "--contour_bias", help="Provide value of contour level bias for automatic contours")
 parser.add_argument("-cl", "--contour_levels", nargs="+", help="Provide sigma levels to plot contours at for sigma contours or MJy/sr values for manual contours")
 parser.add_argument("-d", "--d_range", nargs="+", help="range of data to plot", default=False)
+parser.add_argument("-b", "--boundaries", nargs="+", help="Boundaries of image in pixels", default=False)
 parser.add_argument("-o", "--outputfile", help="name of output .png file (optional)", default=False)
 args = parser.parse_args()
 inputfile=args.inputfile
@@ -31,6 +32,7 @@ contour_file=args.contour_file
 contour_type=args.contour_type
 contour_bias=args.contour_bias
 
+boundaries=args.boundaries
 d_range=args.d_range
 outputfile=args.outputfile
 
@@ -55,60 +57,60 @@ def jy_beam_MJy_sr(data, header):
 
 def import_fits(filename, d_range):
 	"""Imports a FITS radio image from CASA pipeline and returns the image array in units of MJy/sr and the header"""
-	
+
 	fits_image_filename = filename
 	with fits.open(fits_image_filename) as hdul:
 		data=hdul[0].data
 	header = fits.getheader(fits_image_filename)
-	
+
 	ndims = data.ndim
 	if ndims == 4:
 		image=data[0,0,:,:]
 	else:
 		image=data
-	
+
 	ar_u = jy_beam_MJy_sr(image, header)
 	ar_c=mask(ar_u)
 	if d_range != False:
 		array=clip(ar_c, d_range)
 		ar_c=mask(array)
-	
+
 
 	return ar_c,header
 
 
 def mask(array):
 	"""Masks the array so invalid values are discarded"""
-	
+
 	unmasked=np.array(array, dtype=np.float32)
 	masked=np.ma.masked_invalid(unmasked)
 	output=masked
 	return output
 
-	
+
 def clip(array, d_range):
 	"""Clips values outside of the data range, replaces them with max and min values"""
-	
+
 
 	array[(array < np.float32(d_range[0]))] = d_range[0]
 	array[(array > np.float32(d_range[1]))] = d_range[1]
-	
+
 	array=mask(array)
-	
+
 	return array
-		
-	
-def image_plot(inputfile, d_range, outputfile):
+
+
+def image_plot(inputfile, d_range, boundaries, outputfile):
 	"""Takes a scaled image in the form of a numpy array and plots it along with coordinates and a colorbar"""
-	
+
 	block = import_fits(inputfile, d_range)
 	data = block[0]
 	hrd = block[1]
 	wcs = WCS(hrd)
 	if hrd['TELESCOP'] != 'Spitzer':
 		beam = Beam.from_fits_header(hrd)
-		
 
+	print(np.shape(data))
 	norm = ImageNormalize(data, interval=MinMaxInterval(), stretch=SqrtStretch())
 
 	figure=plt.figure(num=1)
@@ -116,27 +118,27 @@ def image_plot(inputfile, d_range, outputfile):
 		ax=figure.add_subplot(111, projection=wcs, slices=('x','y', 0, 0))
 	elif wcs.pixel_n_dim ==2:
 		ax=figure.add_subplot(111, projection=wcs, slices=('x','y'))
-		
+
 
 	main_image=ax.imshow(X=data, cmap='plasma', origin='lower', norm=norm, vmax=np.max(data) , vmin=np.min(data))
 	cbar=figure.colorbar(main_image)
-	
+	if boundaries != False:
+		ax.set_xlim(boundaries[0],boundaries[1])
+		ax.set_ylim(boundaries[2],boundaries[3])
+
 	if hrd['TELESCOP'] == 'Spitzer':
 		ax.invert_xaxis()
 		ax.invert_yaxis()
-		
 
-	coord = SkyCoord('23h23m37s +61:04, frame='J200')
-		
-		
+
 	ax.set_xlabel('Right Ascension J200')
 	ax.set_ylabel('Declination J2000')
 	cbar.set_label('Surface Brigthness (MJy/Sr)')
-	
+
 	if contour_file != False:
 		contours=contour_plot(ax, contour_file, contour_type, beam)
-		
-		
+
+
 	plt.show()
 	if outputfile != False:
 		plt.savefig(os.getcwd()+outputfile)
@@ -148,24 +150,20 @@ def image_plot(inputfile, d_range, outputfile):
 
 def import_contours(filename):
 	"""Imports a FITS radio image from CASA pipeline, and coverts units to MJy/sr if not already in those units."""
-	
+
 	contour_fits = filename
 	with fits.open(contour_fits) as hdul:
 		data=hdul[0].data
 	header = fits.getheader(contour_fits)
-	
+
 	ndims = data.ndim
 	if ndims == 4:
 		image=data[0,0,:,:]
 	else:
 		image=data
-	
+
 	ar_u = jy_beam_MJy_sr(image, header)
 	ar_c=mask(ar_u)
-	if d_range != False:
-		array=clip(ar_c, d_range)
-		ar_c=mask(array)
-	
 
 	return ar_c,header
 
@@ -187,8 +185,8 @@ def contour_plot(ax, contour_file, contour_type, image_beam):
 	if contour_type == "automatic":
 		spacing = contour_bias
 		n = 7 #number of contours
-		ub = np.max(contour_data) 
-		lb = np.min(contour_data) 
+		ub = np.max(contour_data)
+		lb = np.min(contour_data)
 		def level_func(lb, ub, n, spacing=1.1):
 			span = (ub-lb)
 			dx = 1.0 / (n-1)
@@ -206,18 +204,9 @@ def contour_plot(ax, contour_file, contour_type, image_beam):
 	if contour_type == "manual":
 		levels=list(map(float, args.contour_levels))
 		print('Contour Levels are: ', levels, 'MJy/sr')
-		ax.contour(cdata, levels=levels, norm=norm, transform=ax.get_transform(cwcs.celestial), colors='white', alpha=0.5)
-
-
-	
-
-result=image_plot(inputfile, d_range,  outputfile)
+		ax.contour(cdata, levels=levels, norm=norm, transform=ax.get_transform(cwcs.celestial), colors='black', alpha=0.5)
 
 
 
 
-
-
-
-
-
+result=image_plot(inputfile, d_range, boundaries,  outputfile)
