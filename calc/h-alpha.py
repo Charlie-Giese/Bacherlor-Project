907ad99d0141de8ad5dfa2bba802860ed5001e0a
Charlie-Giese <giesec@tcd.ie>
2021-09-29 18:15:39 +0100

    update

--- calc/h-alpha.py
+++ calc/h-alpha.py
@@ -210,7 +210,7 @@ ax3.plot(x, np.log(ratio_2), label='Middle')
 ax3.plot(x, np.log(ratio_3), label='Bottom')
 ax3.set_xlabel('Right Ascension J2000')
 ax3.set_aspect('auto')
-xticks = ax.get_ticks() * u.degree
+xticks = ax3.get_ticks() * u.degree
 print('old ticks are ', xticks)
 xticks_new = xticks.to(u.hms)
 print('new ticks are ', xticks_new)
@@ -218,7 +218,7 @@ tick_list=[]
 for val in xticks_new:
 	tick_list.append(str(val))
 print('strings are ', tick_list)
-ax.set_xticklabels(tick_list)
+ax3.set_xticklabels(tick_list)
 ax3.legend()
 plt.show()
 
